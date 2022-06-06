d7494ce0edee5a94756c2c0247a02227badb3d28
Tiemi9 <cristina_tiemi.social@ayahoo.com>
2022-06-06 16:07:34 -0300

    Add Jump function

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,21 @@
 const dino = document.querySelector('.dino');
 
-function handleKeyUp (event) {
-    if(event.keyCode === 32) /*keyCode = cód. do teclado verificar mais em keyCode.info*/
-    console.log('Apertou espaço');
+function handleKeyUp(event) {
+    if(event.keyCode === 32) {/*keyCode = cód. do teclado verificar mais em keyCode.info*/
+    
+    jump();
+    }
+}
+
+function jump() {
+    let position = 0; //posição inicial
+
+    //pula ao apertat espaço
+    let upInterval = setInterval(() => {
+        position += 20;
+
+        dino.style.bottom = position + 'px';
+    }, 20); //será executado a cada 20ms
 }
 
 document.addEventListener('keyup', handleKeyUp); 
